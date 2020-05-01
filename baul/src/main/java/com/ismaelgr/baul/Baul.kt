7f23de3b04bbda25eafe364c7d6598171bd87bd5
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2020-05-01 15:48:46 +0200

    Fix data has not been initialized and now we check the file reading casting to hashmap

--- baul/src/main/java/com/ismaelgr/baul/Baul.kt
+++ baul/src/main/java/com/ismaelgr/baul/Baul.kt
@@ -5,7 +5,7 @@ import java.io.*
 
 class Baul(projectFilesPath: String) {
 
-    private lateinit var data: HashMap<String, BaseDataSource>
+    private var data: HashMap<String, BaseDataSource> = HashMap()
     private val CACHE_FILENAME = "$projectFilesPath/preloadData.baul"
 
     init {
@@ -13,7 +13,7 @@ class Baul(projectFilesPath: String) {
         createCacheIfNotExists()
     }
 
-    fun setDataSources(vararg dataSource: Pair<String, BaseDataSource>){
+    fun setDataSources(vararg dataSource: Pair<String, BaseDataSource>) {
         Log.i("baulClass: ", "setting data sources");
         dataSource.forEach { pair ->
             data.put(pair.first, pair.second)
@@ -26,9 +26,12 @@ class Baul(projectFilesPath: String) {
         if (cacheFile.length() > 0) {
             val editFile = ObjectInputStream(FileInputStream(cacheFile))
             editFile.readObject()?.run {
-                (this as HashMap<String, HashMap<String, Object>>).forEach { pair ->
-                    data[pair.key]!!.fromCacheData(pair.value)
+                if (this is HashMap<*, *>) {
+                    this.forEach { pair ->
+                        data[pair.key]!!.fromCacheData(pair.value as HashMap<String, Object>)
+                    }
                 }
+
             }
             editFile.close()
         }
